81c510a8af9b582b579424d3501fadba8580b883
liblack99 <andresfc-@hotmail.com>
2025-06-16 10:12:59 -0500

    fixed(clases de los slides)

--- inidicadores/index.js
+++ inidicadores/index.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper(".indicadoresSwiper", {
   slidesPerView: 1.2,
-  spaceBetween: 0,
+  spaceBetween: 16,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
