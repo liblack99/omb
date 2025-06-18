fa797dbb37f188641b552fc8f13ae409e28d7bbc
liblack99 <andresfc-@hotmail.com>
2025-06-18 11:34:43 -0500

    add(diseño de publicaciones)

---
+++ publicacion-categoria-selecionada/index.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper(".categoriaSlideSwiper", {
+  slidesPerView: 1.2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {slidesPerView: 2},
+    1024: {slidesPerView: 3},
+    1280: {slidesPerView: 4},
+  },
+});
